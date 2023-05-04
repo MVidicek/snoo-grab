b2211b2f910cbf4cd1f5dbf6b268c19f9bb2bf26
MVidicek <mvidicek@protonmail.com>
2023-05-04 18:32:53 +0200

    Implement audio/video merge with ffmpeg

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import re
 import requests
 import argparse
+import subprocess
 from praw import Reddit
 
 CLIENT_ID = 'your_client_id'
@@ -26,21 +27,38 @@ def get_video_url(reddit_url):
     submission = reddit.submission(url=reddit_url)
     try:
         video_url = submission.media['reddit_video']['fallback_url']
-        return video_url
+        audio_url = video_url.split("DASH_")[0] + "DASH_audio.mp4"
+        return video_url, audio_url
     except KeyError:
         print("No video found in the provided link.")
-        return None
+        return None, None
+    
+def merge_video_audio(video_file, audio_file, output_file):
+    command = f'ffmpeg -y -i {video_file} -i {audio_file} -c:v copy -c:a aac {output_file}'
+    subprocess.call(command, shell=True)    
       
 def main():
     args = parse_arguments()
     reddit_url = args.url
     output_filename = args.output
 
-    video_url = get_video_url(reddit_url)
+    video_url, audio_url = get_video_url(reddit_url)
 
-    if video_url:
-        download_video(video_url, output_filename)
-        print(f"Video downloaded as {output_filename}")
+    if video_url and audio_url:
+        video_file = "temp_video.mp4"
+        audio_file = "temp_audio.mp4"
+
+        download_video(video_url, video_file)
+        download_video(audio_url, audio_file)
+
+        merge_video_audio(video_file, audio_file, output_filename)
+
+        os.remove(video_file)
+        os.remove(audio_file)
+
+        print(f"Video downloaded with audio as {output_filename}")
+    else:
+        print("Unable to download video and audio.")
 
 
 if __name__ == "__main__":
