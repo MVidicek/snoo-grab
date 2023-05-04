e65e1ed301311fde08380a6bc8b42656166ef801
MVidicek <mvidicek@protonmail.com>
2023-05-04 18:16:52 +0200

    Implement command-line arguments

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import re
 import requests
+import argparse
 from praw import Reddit
 
 CLIENT_ID = 'your_client_id'
@@ -8,6 +9,13 @@ CLIENT_SECRET = 'your_client_secret'
 USER_AGENT = 'video_downloader:v1.0 (by u/Marcuskac)'  # e.g., 'video_downloader:v1.0 (by u/your_username)'
 
 reddit = Reddit(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, user_agent=USER_AGENT)
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Download a video from a Reddit link.')
+    parser.add_argument('-u', '--url', type=str, required=True, help='The Reddit URL containing the video.')
+    parser.add_argument('-o', '--output', type=str, default='video.mp4', help='The output filename for the downloaded video.')
+
+    return parser.parse_args()
 
 def download_video(url, file_name):
     response = requests.get(url)
@@ -24,13 +32,15 @@ def get_video_url(reddit_url):
         return None
       
 def main():
-    reddit_url = ''
+    args = parse_arguments()
+    reddit_url = args.url
+    output_filename = args.output
+
     video_url = get_video_url(reddit_url)
 
     if video_url:
-        file_name = "video.mp4"
-        download_video(video_url, file_name)
-        print(f"Video downloaded as {file_name}")
+        download_video(video_url, output_filename)
+        print(f"Video downloaded as {output_filename}")
 
 
 if __name__ == "__main__":
